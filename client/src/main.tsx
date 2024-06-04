6d5a23cbd0315b029b81edaabdc984520f2ea8ba
OmarSharieff <omarsharief642002@gmail.com>
2024-06-04 12:22:17 +0530

    Clerk Initialized

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {ClerkProvider} from '@clerk/clerk-react'
+
+// Importing the Clerk publishable key from the environment variables
+const PUBLISHABLE_KEY =  import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+// Throwing an error if the publishable key is missing
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key. Please set the VITE_CLERK_PUBLISHABLE_KEY environment variable.');
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>,
 )
